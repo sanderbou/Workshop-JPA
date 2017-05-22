8adf88e12e182195613222b898262764f3622b9c
Tobias Poll <tobias.poll@first8.nl>
2017-05-22 21:45:28 +0200

    Add solution

--- src/main/java/nl/first8/hu/ticketsale/reporting/ReportRepository.java
+++ src/main/java/nl/first8/hu/ticketsale/reporting/ReportRepository.java
@@ -18,7 +18,11 @@ public class ReportRepository {
     }
 
     public List<LocationReport> findGenreLocations(String genre) {
-        //TODO
-       throw new UnsupportedOperationException();
+        String jpql = "SELECT DISTINCT NEW nl.first8.hu.ticketsale.reporting.LocationReport(t.concert.artist, t.concert.location.name, t.account.info.city) " +
+                "FROM Ticket t " +
+                "WHERE t.concert.genre = :genre";
+        TypedQuery<LocationReport> query = entityManager.createQuery(jpql, LocationReport.class);
+        query.setParameter("genre", genre);
+        return query.getResultList();
     }
 }
